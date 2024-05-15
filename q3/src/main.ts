85d2be2cde9314602e72d06f626fe3ba41a69d46
BEAR小熊帽 <1922343892@qq.com>
2024-05-15 14:34:35 +0800

    feat: 初始化提交

--- q3/src/main.ts
+++ q3/src/main.ts
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
+import { Component, computed, createApp, inject, provide, reactive, watchEffect } from 'vue'
 import './reset.css'
+import router from './router/index.js';
+import VueRouter from 'vue-router';
 import App from './App.vue'
+ref == reactive();
+computed
+watch
+watchEffect
+v-if(
+    <load>
+) else {
+    <Component>
+}
+provide inject
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
